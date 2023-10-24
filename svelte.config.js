ed232efc7b3bffc65becfb9fa4f7ecc3d1731fae
Enrico <enricomi@mit.edu>
2023-10-24 08:30:01 -0400

    dynamic reference error

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,9 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-	adapter: adapter(),
+	adapter: adapter({
+	    fallback: '404.html'
+	}),
 	paths: {
 	    // setting path base for github pages deployment
 	    base: process.env.NODE_ENV === 'production' ? '/wikoSB' : '',
